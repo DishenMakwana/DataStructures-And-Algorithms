78ea2bd4a7e50a41427f2a171ff8342dea3a829b
Darshan-Upadhyay <61383886+Darshan-upadhyay1110@users.noreply.github.com>
2020-06-10 19:12:50 +0530

    Simple beginner level recursion problems

--- src/recursion/PracticeSet1.java
+++ src/recursion/PracticeSet1.java
@@ -1,9 +1,11 @@
 package recursion;
 
-public class PracticeSet1 {
+public class Set1 {
 
 	public static void main(String[] args) {
-		System.out.println(sum(5));
+	System.out.println(sum(10));
+	System.out.println(fastPow(3,4));
+	System.out.println((grid(10,2)));
 
 	}
 	static int sum(int n)
@@ -12,10 +14,24 @@ public class PracticeSet1 {
 		{
 			return 1;
 		}
-		else
-		{
-			return sum(n-1)+n;
-		}
+		return sum(n-1)+n;
 	}
-
+	static  int fastPow(int a,int b)
+	{
+		if(b==0)
+		{
+			return 1;
+		}
+		else if(b%2==0)
+		{
+			return fastPow(a*a,(b/2));
+		}
+		return a*fastPow(a,b-1);
+		
+	}
+static int grid(int n,int m)
+{
+	if(n==1 || m==1)return 1;
+	return grid(n,m-1)+grid(n-1,m);
+}
 }
