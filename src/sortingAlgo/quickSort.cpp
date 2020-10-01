fc83df0eebf5d0552cdeadedacb98e634255fe70
DishenMakwana <58837408+DishenMakwana@users.noreply.github.com>
2020-10-01 10:57:37 +0530

    change in user input
    1. now we get length of array from user and define size of array to n.

--- src/sortingAlgo/quickSort.cpp
+++ src/sortingAlgo/quickSort.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-int quickPartition(int *a,int start,int end)
+
+int quickPartition(int a[],int start,int end)
 {
     int pivot=a[end];
     int pIndex=start;
@@ -13,29 +14,31 @@ int quickPartition(int *a,int start,int end)
         }
 
     }
-            swap(a[pIndex],a[end]);
-            return pIndex;
-        
-
+    swap(a[pIndex],a[end]);
+    return pIndex;
 } 
-void quickSort(int *a,int start,int end)
+
+void quickSort(int a[],int start,int end)
 {
-    if(start<end){
-        int pIndex=quickPartition(a,start,end);
-    quickSort(a,start,pIndex-1);
-    quickSort(a,pIndex+1,end);
+    if(start<end)
+    {
+        int pIndex = quickPartition(a,start,end);
+        quickSort(a,start,pIndex-1);
+        quickSort(a,pIndex+1,end);
     }
     
 }
 int main()
 {
-    int a[10];
-    for(int i=0;i<10;i++)
+    int n;
+    cin >> n;
+    int a[n];
+    for(int i=0;i<n;i++)
     {
         cin>>a[i];
     }
-    quickSort(a,0,9);
-    for(int i=0;i<10;i++)
+    quickSort(a,0,n-1);
+    for(int i=0;i<n;i++)
     {
         cout<<a[i]<<" ";
     }
