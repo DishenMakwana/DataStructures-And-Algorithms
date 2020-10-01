c8f3fd5742ab5d7558453512d6cb2f3ce8de2890
DishenMakwana <58837408+DishenMakwana@users.noreply.github.com>
2020-10-01 11:09:28 +0530

    change merge function logic
    1. now take input n from user and array is of size n.
    2. change in logic of mergeSort function.
    3. change in logic of merge function.

--- src/sortingAlgo/mergeSort.cpp
+++ src/sortingAlgo/mergeSort.cpp
@@ -1,74 +1,83 @@
 #include<bits/stdc++.h>
-#include<iostream>
-#include<array>
 using namespace std;
 
-void merge(int *a,int *l,int L,int *r,int R)
+void merge(int a[],int l,int m,int r)
 {
-   
-    int i=0,j=0,k=0;
-    while(i<L && j<R)
-    {
-        if(l[i]<r[j])
-        {
-            a[k++]=l[i++];
-        }
-        else
-        {
-            a[k++]=r[j++];
-        }
+    int n1 = m - l + 1; 
+    int n2 = r - m; 
+    int left[n1], right[n2]; 
+  
+    for (int i = 0; i < n1; i++) 
+        left[i] = a[l + i]; 
+        
+    for (int j = 0; j < n2; j++) 
+        right[j] = a[m + 1 + j]; 
+  
+    int i = 0,j = 0,k = l; 
      
-    }
-    while(i<L)
+    while (i < n1 && j < n2) 
+    { 
+        if (left[i] <= right[j]) 
+        { 
+            a[k] = left[i]; 
+            i++; 
+        } 
+        else 
+        { 
+            a[k] = right[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+  
+    while (i < n1) 
+    { 
+        a[k] = left[i]; 
+        i++; 
+        k++; 
+    } 
+  
+    while (j < n2) 
+    { 
+        a[k] = right[j]; 
+        j++; 
+        k++; 
+    } 
+    
+    /*for(int l=0;l<sizeof(a);l++)
     {
-        a[k++]=l[i++];
+       cout << a[l] << " ";
     }
-    while(j<R)
-    {
-        a[k++]=r[j++];
-    }
+    cout << endl;*/
 }
-////////////////////////////////////////////////
-void mergeSort(int *a,int n)
-{
-    if(n<2)
-    {
-        return;
-    }
-    int mid=n/2;
-    int *l,*r;
-    l=(int*)malloc(mid*sizeof(int));
-    r=(int*)malloc((n-mid)*sizeof(int));
 
-    for(int i=0;i<mid;i++)
-    {
-        l[i]=a[i];
-    }
-    for(int i=mid;i<n;i++)
-    {
-        r[i-mid]=a[i];
-    }
-     
-    mergeSort(l,mid);
-    mergeSort(r,n-mid);
-     merge(a,l,mid,r,n-mid);
-     free(l);
-     free(r);
-}
-////////////////////////////////////////////////
-int main()
+void mergeSort(int a[],int l,int r)
 {
-    int n=10;
+    if(l<r)
+    {
+      int mid = l + (r - l) / 2; 
+      mergeSort(a, l, mid); 
+      mergeSort(a, mid + 1, r); 
+      merge(a, l, mid, r); 
+    } 
+}
+
+int main()
+{   
+    int n;
+    cin >> n;
     int a[n];
+   
     for(int i=0;i<n;i++)
     {
         cin >> a[i] ;
     }
-    mergeSort(a,n);
+   
+    mergeSort(a,0,n);
+   
     for(int i=0;i<n;i++)
     {
         cout<<a[i]<<" ";
     }
-
-return 0;
+    return 0;
 }
